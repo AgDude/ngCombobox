360a69e51ca47490166bbce5126d393c407a0ac0
ndudenhoeffer <nate@dudesag.com>
2014-07-19 06:50:40 -0500

    don't allow tags to be set to undefined.

--- src/combobox.js
+++ src/combobox.js
@@ -275,9 +275,12 @@ ngCombobox.directive('combobox', function ($timeout, $document, $sce, $q, grep, 
         
         scope.$watch('tags', function (value) {
           if ( value === undefined || value === null || (value.length>0 && value[0] === undefined) ){
+            // If it is undefined, revert the change
             scope.tags = scope.tagList.items;
           }
-          else{ scope.tags = makeObjectArray(value, options.displayProperty); }
+          else{ 
+            scope.tags = makeObjectArray(value, options.displayProperty);
+          }
           scope.tagList.items = scope.tags;
         });
 
